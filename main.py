20ed26ff43c317e0b555a1273383a6c484e7c927
sjtommylee <sjtommylee@gmail.com>
2021-08-24 20:46:48 -0700

    wip

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import Optional
 from fastapi import FastAPI
 # importing the basemodel from pydantic, which we need for the req.body
 from pydantic import BaseModel
+import uvicorn
 # create an instance of fastapi
 app = FastAPI()
 
@@ -52,3 +53,7 @@ class Blog(BaseModel):
 def create_blog(request: Blog):
     print({'data': {'title': {request.title}, "body": {request.body}}})
     return {'data': {'title': {request.title}, "body": {request.body}}}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
